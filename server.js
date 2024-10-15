1b056f5e3a50e7f2d140cc27b79ee093c13e8fe7
OmkarSawantt <omkarsawantttt4@gmail.com>
2024-10-15 19:15:56 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+const app = express();
+app.use(cors())
+app.use(fileUpload());
+
+app.get("/download", (req, res) => {
+  const fileName = "data.txt"; 
+  const directoryPath = path.join(__dirname, "uploads");
+  const filePath = path.join(directoryPath, fileName);
+
+  res.download(filePath, fileName, (err) => {
+    if (err) {
+      console.log("Error downloading file:", err);
+      res.status(500).send("File could not be downloaded.");
+    }
+  });
+});
+
+
+
+app.listen(4000, () => {
+  console.log(`Server Running On 4000`);
+});
