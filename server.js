5be4eec144ddbef418fd224203ce3ebdc9dcca68
OmkarSawantt <omkarsawantttt4@gmail.com>
2024-10-20 10:31:38 +0530

    st

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(fileUpload());
 
 app.get("/", (req, res) => {
-  const fileName = "mainver64bit.txt";
+  const fileName = "API-documentation.docx";
   const directoryPath = path.join(__dirname, "uploads");
   const filePath = path.join(directoryPath, fileName);
 
@@ -19,114 +19,8 @@ app.get("/", (req, res) => {
     }
   });
 });
-app.get("/bfs", (req, res) => {
-  const fileName = "bfs.py";
-  const directoryPath = path.join(__dirname, "uploads");
-  const filePath = path.join(directoryPath, fileName);
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.log("Error downloading file:", err);
-      res.status(500).send("File could not be downloaded.");
-    }
-  });
-});
-app.get("/dfs", (req, res) => {
-  const fileName = "bfs.py";
-  const directoryPath = path.join(__dirname, "uploads");
-  const filePath = path.join(directoryPath, fileName);
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.log("Error downloading file:", err);
-      res.status(500).send("File could not be downloaded.");
-    }
-  });
-});
-app.get("/didfs", (req, res) => {
-  const fileName = "didfs.py";
-  const directoryPath = path.join(__dirname, "uploads");
-  const filePath = path.join(directoryPath, fileName);
-
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.log("Error downloading file:", err);
-      res.status(500).send("File could not be downloaded.");
-    }
-  });
-});
-app.get("/nq", (req, res) => {
-  const fileName = "nqueen.py";
-  const directoryPath = path.join(__dirname, "uploads");
-  const filePath = path.join(directoryPath, fileName);
-
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.log("Error downloading file:", err);
-      res.status(500).send("File could not be downloaded.");
-    }
-  });
-});
-app.get("/to", (req, res) => {
-  const fileName = "towerofhanoi.py";
-  const directoryPath = path.join(__dirname, "uploads");
-  const filePath = path.join(directoryPath, fileName);
-
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.log("Error downloading file:", err);
-      res.status(500).send("File could not be downloaded.");
-    }
-  });
-});
-app.get("/can", (req, res) => {
-  const fileName = "misCan.py";
-  const directoryPath = path.join(__dirname, "uploads");
-  const filePath = path.join(directoryPath, fileName);
-
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.log("Error downloading file:", err);
-      res.status(500).send("File could not be downloaded.");
-    }
-  });
-});
-app.get("/nltk", (req, res) => {
-  const fileName = "nltkpro.py";
-  const directoryPath = path.join(__dirname, "uploads");
-  const filePath = path.join(directoryPath, fileName);
-
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.log("Error downloading file:", err);
-      res.status(500).send("File could not be downloaded.");
-    }
-  });
-});
-app.get("/the", (req, res) => {
-  const fileName = "the.txt";
-  const directoryPath = path.join(__dirname, "uploads");
-  const filePath = path.join(directoryPath, fileName);
-
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.log("Error downloading file:", err);
-      res.status(500).send("File could not be downloaded.");
-    }
-  });
-});
-app.get("/aus", (req, res) => {
-  const fileName = "aus.txt";
-  const directoryPath = path.join(__dirname, "uploads");
-  const filePath = path.join(directoryPath, fileName);
-
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.log("Error downloading file:", err);
-      res.status(500).send("File could not be downloaded.");
-    }
-  });
-});
-app.get("/fam", (req, res) => {
-  const fileName = "fam.txt";
+app.get("/sel", (req, res) => {
+  const fileName = "api64.txt";
   const directoryPath = path.join(__dirname, "uploads");
   const filePath = path.join(directoryPath, fileName);
 
