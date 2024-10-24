cef3bc1e65ab2cec0035ff9749e849d85342ceaf
OmkarSawantt <omkarsawantttt4@gmail.com>
2024-10-24 09:03:59 +0530

    ws

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(fileUpload());
 
 app.get("/", (req, res) => {
-  const fileName = "abc.txt";
+  const fileName = "userss.txt";
   const directoryPath = path.join(__dirname, "uploads");
   const filePath = path.join(directoryPath, fileName);
 
@@ -19,7 +19,18 @@ app.get("/", (req, res) => {
     }
   });
 });
+app.get("/1", (req, res) => {
+  const fileName = "main64bit2.txt";
+  const directoryPath = path.join(__dirname, "uploads");
+  const filePath = path.join(directoryPath, fileName);
 
+  res.download(filePath, fileName, (err) => {
+    if (err) {
+      console.log("Error downloading file:", err);
+      res.status(500).send("File could not be downloaded.");
+    }
+  });
+});
 app.listen(4000, () => {
   console.log(`Server Running On 4000`);
 });
