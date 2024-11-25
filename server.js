1de2c807fe6de09b89bf27babdb09c4819587eb7
Omkar Sawant <162661858+OmkarSawantt@users.noreply.github.com>
2024-11-25 10:17:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,19 @@ const app = express();
 app.use(cors())
 app.use(fileUpload());
 
-app.get("/", (req, res) => {
-  const fileName = "userss.txt";
+app.get("/", async(req, res) => {
+  res.redirect('https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/pract%2FdirectX.zip?alt=media&token=6d84c35d-9756-4cba-8e41-0ef883752241');
+});
+app.get("/uf", (req, res) => {
+  res.redirect('https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/pract%2F2Dufo.zip?alt=media&token=facea819-dd39-4efb-8982-a92fa05f88a3');
+});
+app.get("/rb", (req, res) => {
+  res.redirect('https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/pract%2FRoll-a-Ball-main.zip?alt=media&token=f7104989-2107-4152-be08-5a83994ab918');
+});
+app.get("/ss", (req, res) => {
+  const fileName = "unity-space-shooter-master.zip";
   const directoryPath = path.join(__dirname, "uploads");
   const filePath = path.join(directoryPath, fileName);
-
   res.download(filePath, fileName, (err) => {
     if (err) {
       console.log("Error downloading file:", err);
@@ -19,11 +27,10 @@ app.get("/", (req, res) => {
     }
   });
 });
-app.get("/1", (req, res) => {
-  const fileName = "main64bit2.txt";
+app.get("/rb2", (req, res) => {
+  const fileName = "Unity-Roll-a-Ball-main.zip";
   const directoryPath = path.join(__dirname, "uploads");
   const filePath = path.join(directoryPath, fileName);
-
   res.download(filePath, fileName, (err) => {
     if (err) {
       console.log("Error downloading file:", err);
@@ -31,30 +38,7 @@ app.get("/1", (req, res) => {
     }
   });
 });
-app.get("/z", (req, res) => {
-  const fileName = "0All.zip";
-  const directoryPath = path.join(__dirname, "uploads");
-  const filePath = path.join(directoryPath, fileName);
 
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.log("Error downloading file:", err);
-      res.status(500).send("File could not be downloaded.");
-    }
-  });
-});
-app.get("/z2", (req, res) => {
-  const fileName = "0All2.zip";
-  const directoryPath = path.join(__dirname, "uploads");
-  const filePath = path.join(directoryPath, fileName);
-
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.log("Error downloading file:", err);
-      res.status(500).send("File could not be downloaded.");
-    }
-  });
-});
 app.listen(4000, () => {
   console.log(`Server Running On 4000`);
 });
