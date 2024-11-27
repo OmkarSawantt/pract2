3331c669153a79d28689e014dd591a905c36a868
Omkar Sawant <162661858+OmkarSawantt@users.noreply.github.com>
2024-11-27 09:07:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,36 +8,10 @@ app.use(cors())
 app.use(fileUpload());
 
 app.get("/", async(req, res) => {
-  res.redirect('https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/pract%2FdirectX.zip?alt=media&token=6d84c35d-9756-4cba-8e41-0ef883752241');
+  res.redirect('https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/pract%2FAll.py?alt=media&token=e11c0c61-6f5d-4e1e-a120-38e6566916df');
 });
-app.get("/uf", (req, res) => {
-  res.redirect('https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/pract%2F2Dufo.zip?alt=media&token=facea819-dd39-4efb-8982-a92fa05f88a3');
-});
-app.get("/rb", (req, res) => {
-  res.redirect('https://firebasestorage.googleapis.com/v0/b/uploadingfile-1f51f.appspot.com/o/pract%2FRoll-a-Ball-main.zip?alt=media&token=f7104989-2107-4152-be08-5a83994ab918');
-});
-app.get("/ss", (req, res) => {
-  const fileName = "unity-space-shooter-master.zip";
-  const directoryPath = path.join(__dirname, "uploads");
-  const filePath = path.join(directoryPath, fileName);
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.log("Error downloading file:", err);
-      res.status(500).send("File could not be downloaded.");
-    }
-  });
-});
-app.get("/rb2", (req, res) => {
-  const fileName = "Unity-Roll-a-Ball-main.zip";
-  const directoryPath = path.join(__dirname, "uploads");
-  const filePath = path.join(directoryPath, fileName);
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.log("Error downloading file:", err);
-      res.status(500).send("File could not be downloaded.");
-    }
-  });
-});
+
+
 
 app.listen(4000, () => {
   console.log(`Server Running On 4000`);
