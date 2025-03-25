802c34557df461c866f9ae24a56832f8bf106cc0
Omkar Sawant <162661858+OmkarSawantt@users.noreply.github.com>
2025-03-25 13:54:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -72,7 +72,17 @@ app.get("/me", async(req, res) => {
   res.redirect('https://rb.gy/chd0oo');
 });
 
-
+app.get("/cal", async(req, res) => {
+  const fileName = "Cloud.txt";
+  const directoryPath = path.join(__dirname, "uploads");
+  const filePath = path.join(directoryPath, fileName);
+  res.download(filePath, fileName, (err) => {
+    if (err) {
+      console.log("Error downloading file:", err);
+      res.status(500).send("File could not be downloaded.");
+    }
+  });
+});
 
 app.listen(4000, () => {
   console.log(`Server Running On 4000`);
